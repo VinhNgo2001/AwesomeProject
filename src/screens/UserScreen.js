4c676ccca3b0b0339a2780667ec65fd318ba26c9
VinhNgo2001 <114270673+VinhNgo2001@users.noreply.github.com>
2023-05-27 06:54:08 +0700

    call api user

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from 'react';
 import Colors from '../constants/Colors'
 import ButtonLogin from '../components/ButtonLogin'
 
+
 const URL= 'http://172.20.33.125:5000/api/v1/users'
 
 
@@ -12,27 +13,28 @@ const URL= 'http://172.20.33.125:5000/api/v1/users'
 
 
 const UserScreen = ({navigation}) => {
-  const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const getDataUser = async () => {
+  const [isLoading, setLoading] = useState(true);
+  const callDataUser  = async()=>{
+    console.log('calling...')
     try {
-      const response = await fetch(
-        'http://172.20.33.125:5000/api/v1/users',
-      );
+      const response = await fetch('http://172.20.33.125:5000/api/v1/users/10');
       const json = await response.json();
-      // console.log('check data: ', json)
-      setData(json.data)
+      setData(json);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
-    
-  };
-
+  }
   useEffect(() => {
-    getDataUser();
+    callDataUser();
   }, []);
-  let dataUser= data[0]
-  console.log('data:name ', dataUser)
+  let data1=data
+  // console.log('check: ',data['data']["id"])
+  
+  
+  
   return (
    
       <View style={styles.container}>
@@ -43,18 +45,19 @@ const UserScreen = ({navigation}) => {
           style={{width:200,height:200, borderRadius:100,marginTop:30}}
           ></Image>
         </View>
-        <View style={{marginLeft:10}}>  
-        <Text style={styles.textUser}>
-          Name :{dataUser.firstName}
-        </Text>
+        <View style={{marginLeft:10}}> 
+        {isLoading ? <ActivityIndicator/>:(<Text style={styles.textUser}>
+         Name:  {data['data']["firstName"]}
+        </Text>)}
+        
         <Text style={styles.textUser}>
           Date of Birth: April 22, 2016
         </Text>
         <Text style={styles.textUser}>
-          Email: Oggy*****@gmail.com
+          Email: {data['data']["email"]}
         </Text>
         <Text style={styles.textUser}>
-          Phone: *******89
+          Phone: {data['data']["numberPhone"]}
         </Text>
       </View>
       <View style={{alignItems:"center", marginTop:20}}>
